cd5053fbc4f355e2a0295d5c1ac9aa3574c3eea8
Mona Lisa <kajman5021@gmail.com>
2025-10-02 17:02:16 +0200

    cghfg

--- utils/emailSender.js
+++ utils/emailSender.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 // Konfiguracja transportera
 const createTransporter = () => {
-  return nodemailer.createTransporter({
+  return nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
     secure: false, // true dla 465, false dla innych portów
