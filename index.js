941d6bd648d4a443ed5324f9eaa105a34ebdd968
Mona Lisa <kajman5021@gmail.com>
2025-10-07 13:00:59 +0200

    afsdfsda

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'https://m2notarialnie.pl',
   credentials: true // Ważne dla cookies
 }));
 
